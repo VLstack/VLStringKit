f4116294b8ca373ae1e9a74557a4e2eebeef27cf
VLstack <github@vlstack.fr>
2024-08-19 09:34:53 +0200

    add func "pluralized"

--- Sources/VLStringKit/VLStringKit.swift
+++ Sources/VLStringKit/VLStringKit.swift
@@ -2,6 +2,19 @@ import Foundation
 
 public extension String
 {
+ func pluralized(count: Double) -> String
+ {
+  let inflected: String = String(AttributedString(localized: "^[\(count) \(self)](inflect: true)").characters)
+  let replaceTarget: String = String(AttributedString(localized: "^[\(count)](inflect: true)").characters)
+  
+  return inflected.replacingOccurrences(of: replaceTarget, with: "").trim()
+ }
+ 
+ func pluralized(count: Int) -> String
+ {
+  self.pluralized(count: Double(count))
+ }
+
  func trim(_ set: CharacterSet = .whitespacesAndNewlines) -> String
  {
   self.trimmingCharacters(in: set)
