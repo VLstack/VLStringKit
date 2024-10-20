38a5bb9aa5ba0c3bff4f7f3a266c03b833dd0eb2
VLstack <github@vlstack.fr>
2024-10-20 11:24:30 +0200

    Add firstCharacterCapitalized extension to String

---
+++ Sources/VLStringKit/firstCharacterCapitalized.swift
@@ -0,0 +1,15 @@
+extension String
+{
+ var firstCharacterCapitalized: String
+ {
+  /// Returns a new string with the first character capitalized and the remaining characters in lowercase.
+  ///
+  /// If the string is empty, it returns the original string.
+  ///
+  /// - Returns: A string with the first character capitalized and the rest in lowercase.
+  guard let firstLetter = self.first?.uppercased()
+  else { return self }
+
+  return firstLetter + self.dropFirst().lowercased()
+ }
+}
