6069c7659d7218526cc26651f0c22da0e41c7d4b
VLstack <github@vlstack.fr>
2025-09-21 15:14:36 +0200

    add @inlinable annotation

--- Sources/VLStringKit/trim.swift
+++ Sources/VLStringKit/trim.swift
@@ -6,6 +6,7 @@ public extension String
  ///
  /// - Parameter set: A character set to trim. Defaults to .whitespacesAndNewlines.
  /// - Returns: A trimmed string with specified characters removed from both ends.
+ @inlinable
  func trim(_ set: CharacterSet = .whitespacesAndNewlines) -> String
  {
   self.trimmingCharacters(in: set)
@@ -14,6 +15,7 @@ public extension String
  /// A version of the string with leading whitespaces, trailing whitespaces and newlines removed.
  ///
  /// - Returns: A trimmed string
+ @inlinable
  var trimmed: String
  {
   self.trim()
